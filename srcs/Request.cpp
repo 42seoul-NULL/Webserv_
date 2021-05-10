ba1d0d19dcd8826b544427adb1ec263c137d3ef2
Junghyo Yang <juyang@c6r6s2.42seoul.kr>
2021-05-10 19:52:10 +0900

    [FEAT]: Request Default Class Declare
    Request Default 클래스 작성
    기본 Header 멤버 변수, Getter/Setter, Default Header 생성 Method

---
+++ srcs/Request.cpp
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Request.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juyang <juyang@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 18:39:16 by juyang            #+#    #+#             */
+/*   Updated: 2021/05/10 18:39:16 by juyang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Request.hpp"
+
+Request::Request(void) : accept_charsets(""), accept_language(""), authorization(""), content_length(""), content_type(""), date(""), host(""), referer(""), transfer_encoding(""), user_agent("")
+{
+
+}
+
+///////////////////////////////////////////////////////////////
+/////////////       Request Class Getter        ///////////////
+///////////////////////////////////////////////////////////////
+
+const std::string&	Request::getAcceptCharsets(void) const
+{
+	return (this->accept_charsets);
+}
+
+const std::string&	Request::getAcceptLanguage(void) const
+{
+	return (this->accept_language);
+}
+
+const std::string&	Request::getAuthorization(void) const
+{
+	return (this->content_length);
+}
+
+const std::string&	Request::getContentLength(void) const
+{
+	return (this->content_length);
+}
+
+const std::string&	Request::getContentType(void) const
+{
+	return (this->content_type);
+}
+
+const std::string&	Request::getDate(void) const
+{
+	return (this->date);
+}
+
+const std::string&	Request::getHost(void) const
+{
+	return (this->host);
+}
+
+const std::string&	Request::getReferer(void) const
+{
+	return (this->referer);
+}
+
+const std::string&	Request::getTransferEncoding(void) const
+{
+	return (this->getTransferEncoding);
+}
+
+const std::string&	Request::getUserAgent(void) const
+{
+	return (this->user_agent);
+}
+
+///////////////////////////////////////////////////////////////
+/////////////       Request Class Setter        ///////////////
+///////////////////////////////////////////////////////////////
+
+void	Request::setAcceptCharsets(const std::string& accept_charsets)
+{
+	this->accept_charsets = accept_charsets;
+}
+
+void	Request::setAcceptLanguage(const std::string& accept_language)
+{
+	this->accept_language = accept_language;
+}
+
+void	Request::setAuthorization(const std::string& authorization)
+{
+	this->authorization = authorization;
+}
+
+void	Request::setContentLength(const std::string& content_length)
+{
+	this->content_length = content_length;
+}
+
+void	Request::setContentType(const std::string& content_type)
+{
+	this->content_type = content_type;
+}
+
+void	Request::setDate(const std::string& date)
+{
+	this->date = date;
+}
+
+void	Request::setHost(const std::string& host)
+{
+	this->host = host;
+}
+
+void	Request::setReferer(const std::string& referer)
+{
+	this->referer = referer;
+}
+
+void	Request::setTransferEncoding(const std::string& transfer_encoding)
+{
+	this->transfer_encoding = transfer_encoding;
+}
+
+void	Request::setUserAgent(const std::string& user_agent)
+{
+	this->user_agent = user_agent;
+}
+
+///////////////////////////////////////////////////////////////
+/////////////         Request Variable          ///////////////
+///////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////
+/////////////          Request Header           ///////////////
+///////////////////////////////////////////////////////////////
+
+std::string&	Request::createRequestHeader()
+{
+	std::string	header = std::string("");
+
+	header += "Aceept-Charset: " + this->accept_charsets + "\r\n";
+	header += "Accept-Language: " + this->accept_language + "\r\n";
+	header += "Authorization: " + this->authorization + "\r\n";
+	header += "Content-Length: " + this->content_length + "\r\n";
+	header += "Content-Type: " + this->content_type + "\r\n";
+	header += "Date: " + this->date + "\r\n";
+	header += "Host: " + this->host + "\r\n";
+	header += "Referer: " + this->referer + "\r\n";
+	header += "Transfer-Encoding: " + this->transfer_encoding + "\r\n";
+	header += "User-Agent: " + this->user_agent + "\r\n";
+	header += "\r\n";
+
+	return (header);
+}
