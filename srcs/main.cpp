c8358c8c3c3ea12788ed6a1f5484ca4512208809
Junghyo Yang <juyang@c5r9s3.42seoul.kr>
2021-05-13 15:27:08 +0900

    [REFACTOR]: Response Main Main Test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,10 +11,17 @@
 /* ************************************************************************** */
 
 #include "../libft_cpp/libft.h"
-#include "../includes/parse.hpp"
+// #include "../includes/parse.hpp"
+#include "../includes/Request.hpp"
 
 int	main()
 {
+	std::string fake_request = "GET /tutorials/other/top-20-mysql-best-practices/ HTTP/1.1\r\nHost: net.tutsplus.com\r\nUser-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-us,en;q=0.5\r\nAccept-Encoding: gzip,deflate\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\nKeep-Alive: 300\r\nConnection: keep-alive\r\nCookie: PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120\r\nPragma: no-cache\r\nCache-Control: no-cache\r\n\r\n";
 
-	std::cout << ft_strlen("hello\n!!") << std::endl;
+	Request	test_request;
+	test_request.initRequest();
+	test_request.generateRequest(fake_request);
+	std::cout << test_request.createRawRequest();
+
+	// std::cout << ft_strlen("hello\n!!") << std::endl;
 }
